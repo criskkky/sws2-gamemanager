39d47d8974986fcc5a4d8f1c01bee521f28cf5e9
criskkky <36176343+criskkky@users.noreply.github.com>
2026-02-23 17:17:49 -0300

    chore(src): update version

--- src/Metadata.cs
+++ src/Metadata.cs
@@ -4,7 +4,7 @@ namespace GameManager;
 
 [PluginMetadata(
     Id = "GameManager",
-    Version = "1.0.0",
+    Version = "1.0.1",
     Name = "GameManager",
     Author = "criskkky",
     Description = "Setup your game as you want.",
